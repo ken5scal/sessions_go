abe6994f8795767b458cc35a5f54ae186cb969cb
ken5scal <kengoscal@gmail.com>
2017-02-04 13:22:06 +0900

    Fix type error

--- main.go
+++ main.go
@@ -9,32 +9,32 @@ func init() {
 }
 
 func main() {
-	http.HandlerFunc("/", index)
-	http.HandlerFunc("/bar", bar)
-	http.HandlerFunc("/signup", signup)
-	http.HandlerFunc("/login", login)
-	http.HandlerFunc("/logout", logout)
+	http.HandleFunc("/", index)
+	http.HandleFunc("/bar", bar)
+	http.HandleFunc("/signup", signup)
+	http.HandleFunc("/login", login)
+	http.HandleFunc("/logout", logout)
 	http.Handle("/favicon.ico", http.NotFoundHandler())
 	http.ListenAndServe("port", nil)
 }
 
-func index(w http.Response, req *http.Request) {
+func index(w http.ResponseWriter, req *http.Request) {
 
 }
 
-func bar(w http.Response, req *http.Request) {
+func bar(w http.ResponseWriter, req *http.Request) {
 
 }
 
-func signup(w http.Response, req *http.Request) {
+func signup(w http.ResponseWriter, req *http.Request) {
 
 }
 
-func login(w http.Response, req *http.Request) {
+func login(w http.ResponseWriter, req *http.Request) {
 
 }
 
-func logout(w http.Response, req *http.Request) {
+func logout(w http.ResponseWriter, req *http.Request) {
 
 }
 
